8305cd80e5876b75537688a8bf28b2b33d310e88
Kiet Tran <kiet.tran@dfogpc.com>
2024-05-10 18:02:21 +0700

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+function ObjectId(id) { return id;}
+function ISODate(d) {return d;}
+
+var isDate = function(date) {
+    return (new Date(date) !== "Invalid Date") && !isNaN(new Date(date));
+}
+
+convert = (obj, parent) => {
+  try {
+    if (obj === null) {
+      return null;
+    }
+
+    if (obj.constructor === Array) {
+        return `new BsonArray { ${obj.map(x =>  convert(x))} }`;
+      }
+
+      if (obj.constructor !== Object) {
+        if(obj.constructor === Boolean) return obj;
+        return `"${obj}"`;
+      }
+
+    const keys = Object.keys(obj);
+    let res = "";
+    for (const key of keys) {
+      let val = convert(obj[key]);
+      if (parent !== null) {
+        res += `new BsonDocument { { "${key}", ${val} } }`;
+      } else {
+        res += `{"${key}", ${val}}, `;
+      }
+    }
+    return res;
+  } catch (error) {}
+};
+
+const ToBson = (obj) => convert(obj.constructor === String ? eval('(' + obj + ')') : obj, null);
+export default ToBson;
